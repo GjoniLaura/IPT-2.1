806e254e0ff2f0c878a277dc77d600f8e4fac80c
GjoniLaura <135100122+GjoniLaura@users.noreply.github.com>
2023-07-03 12:49:10 +0200

    Add files via upload

---
+++ GridValue.cs
@@ -0,0 +1,10 @@
+﻿namespace ProjektSnake
+{
+    public enum GridValue
+    {
+        Empty,
+        Snake,
+        Food,
+        Outside
+    }
+}
---
+++ MediaPlayer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+
+namespace ProjektSnake
+{
+    // This is a sound that sounds when the snake eats the food
+    MediaPlayer player = new MediaPlayer(); 
+    player.Open(new Uri("Assets/Eating-Sound.wav", UriKind.Relative));
+    player.Play();
+        
+}
